1ad2220d037ae1f50ee847f7dcbed9d8eb40e837
Clancy <yw3733@columbia.edu>
2023-12-27 01:01:57 -0500

    client side login and registration

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
-import { Activity } from "../models/activity";
 import { toast } from "react-toastify";
+import { Activity } from "../models/activity";
+import { User, UserFormValues } from "../models/user";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
 
@@ -9,10 +10,15 @@ const sleep = (delay: number) => {
     setTimeout(resolve, delay);
   });
 };
-
-axios.defaults.baseURL = "http://localhost:6627/api";
+axios.defaults.baseURL = "http://localhost:5000/api";
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.request.use((config) => {
+  const token = store.commonStore.token;
+  if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 axios.interceptors.response.use(
   async (response) => {
@@ -23,20 +29,17 @@ axios.interceptors.response.use(
     const { data, status, config } = error.response as AxiosResponse;
     switch (status) {
       case 400:
-        if (
-          config.method === "get" &&
-          Object.prototype.hasOwnProperty.call(data.errors, "id")
-        ) {
+        if (config.method === "get" && data.errors.hasOwnProperty("id")) {
           router.navigate("/not-found");
         }
         if (data.errors) {
-          const modelStateErrors = [];
+          const modalStateErrors = [];
           for (const key in data.errors) {
             if (data.errors[key]) {
-              modelStateErrors.push(data.errors[key]);
+              modalStateErrors.push(data.errors[key]);
             }
           }
-          throw modelStateErrors.flat();
+          throw modalStateErrors.flat();
         } else {
           toast.error(data);
         }
@@ -58,7 +61,6 @@ axios.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-
 const requests = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
   post: <T>(url: string, body: {}) =>
@@ -66,16 +68,25 @@ const requests = {
   put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
   del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
-
 const Activities = {
-  list: () => requests.get<Activity[]>("/activities"),
-  details: (id: string) => requests.get<Activity>(`/activity/${id}`),
-  create: (activity: Activity) => requests.post<void>("/activities/", activity),
+  list: () => requests.get<Activity[]>(`/activities`),
+  details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+  create: (activity: Activity) => requests.post<void>(`/activities`, activity),
   update: (activity: Activity) =>
     requests.put<void>(`/activities/${activity.id}`, activity),
-  delete: (id: string) => requests.del<void>(`/activity/${id}`),
+  delete: (id: string) => requests.del<void>(`/activities/${id}`),
 };
 
-const agent = { Activities };
+const Account = {
+  current: () => requests.get<User>("account"),
+  login: (user: UserFormValues) => requests.post<User>("/account/login", user),
+  register: (user: UserFormValues) =>
+    requests.post<User>("/account/register", user),
+};
+
+const agent = {
+  Activities,
+  Account,
+};
 
 export default agent;
